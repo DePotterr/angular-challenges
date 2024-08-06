6f4043da410783876904ae77f46903b915511550
Robin de Potter <robindepotter@outlook.com>
2024-08-06 10:57:48 +0200

    feat: answer-21-anchor-navigation

--- apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
+++ apps/angular/21-anchor-navigation/src/app/nav-button.component.ts
@@ -1,17 +1,20 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'nav-button',
   standalone: true,
   template: `
-    <a [href]="href">
+    <a [routerLink]="href()" [fragment]="fragment()">
       <ng-content></ng-content>
     </a>
   `,
+  imports: [RouterModule],
   host: {
     class: 'block w-fit border border-red-500 rounded-md p-4 m-2',
   },
 })
 export class NavButtonComponent {
-  @Input() href = '';
+  href = input<string>('href');
+  fragment = input<string>('fragment');
 }
