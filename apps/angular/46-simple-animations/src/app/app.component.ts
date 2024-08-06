1899901106775429212afbdf7a6edc773de5e8ba
Robin de Potter <robindepotter@outlook.com>
2024-08-06 12:29:16 +0200

    feat: answer-46-simple-animations

--- apps/angular/46-simple-animations/src/app/app.component.ts
+++ apps/angular/46-simple-animations/src/app/app.component.ts
@@ -1,9 +1,38 @@
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
   imports: [],
   selector: 'app-root',
+  animations: [
+    trigger('fadeInAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(-100px)' }),
+        animate('400ms ease-out', style({ opacity: 1, transform: '*' })),
+      ]),
+    ]),
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(
+          ':leave',
+          [stagger(100, [animate('0.5s', style({ opacity: 0 }))])],
+          { optional: true },
+        ),
+        query(':enter', [
+          style({ opacity: 0 }),
+          stagger(100, [animate('0.5s', style({ opacity: 1 }))]),
+        ]),
+      ]),
+    ]),
+  ],
   styles: `
     section {
       @apply flex flex-1 flex-col gap-5;
@@ -19,7 +48,7 @@ import { Component } from '@angular/core';
   `,
   template: `
     <div class="mx-20 my-40 flex gap-5">
-      <section>
+      <section @fadeInAnimation>
         <div>
           <h3>2008</h3>
           <p>
@@ -51,38 +80,23 @@ import { Component } from '@angular/core';
         </div>
       </section>
 
-      <section>
-        <div class="list-item">
-          <span>Name:</span>
-          <span>Samuel</span>
-        </div>
-
-        <div class="list-item">
-          <span>Age:</span>
-          <span>28</span>
-        </div>
-
-        <div class="list-item">
-          <span>Birthdate:</span>
-          <span>02.11.1995</span>
-        </div>
-
-        <div class="list-item">
-          <span>City:</span>
-          <span>Berlin</span>
-        </div>
-
-        <div class="list-item">
-          <span>Language:</span>
-          <span>English</span>
-        </div>
-
-        <div class="list-item">
-          <span>Like Pizza:</span>
-          <span>Hell yeah</span>
-        </div>
+      <section [@listAnimation]="6">
+        @for (item of list; track item.key) {
+          <div class="list-item">
+            <span>{{ item.key }}:</span>
+            <span>{{ item.value }}</span>
+          </div>
+        }
       </section>
     </div>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly list: { key: string; value: string }[] = [
+    { key: 'Name', value: 'Samuel' },
+    { key: 'Age', value: '28' },
+    { key: 'Birthdate', value: 'City' },
+    { key: 'Language', value: 'English' },
+    { key: 'Like Pizza', value: 'Hell yeah' },
+  ];
+}
